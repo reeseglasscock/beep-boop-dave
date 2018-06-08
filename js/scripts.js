2e94b834845f41449e7381c0e41c758360373aa1
reeseglasscock <reesegpdx@gmail.com>
2018-06-08 16:45:02 -0700

    remove console logs from javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//Business Logic
+
 function roboticTranslation(number){
   var newArray = []
   var roboticDigits = number.split("")
@@ -5,19 +7,15 @@ function roboticTranslation(number){
   for (var digit = 0; digit < roboticDigits.length; digit++) {
     if (number % 3 == 0 && number != 0) {
       newArray.push("I’m sorry, Dave. I’m afraid I can’t do that.")
-      break
     }
     else if (roboticDigits[digit] == 1) {
       newArray.push("Boop!");
-      break
     }
     else if (roboticDigits[digit] == 0) {
       newArray.push("Beep!")
-      break
     }
     else {
       newArray.push(number)
-      break
     }
   }
   return newArray
@@ -26,21 +24,18 @@ function roboticTranslation(number){
 function stringCreatorForNumbers(number){
   var integerWholeNumber = Number(number)
   var integerArray = []
-
   for (number = 0; number <= integerWholeNumber; number++){
     integerArray.push(roboticTranslation(number.toString()))
   }
   return integerArray
-  console.log("This is now the integer Array:" + integerArray)
 }
 
-
+//User Interface
 $(function() {
   $("#robotTalkForm").submit(function(event){
     event.preventDefault();
     var userInput = $("#userInput").val();
     stringCreatorForNumbers(userInput)
-    console.log(stringCreatorForNumbers(userInput))
     $("span#outputToUser").text(stringCreatorForNumbers(userInput).join(" "))
   })
 })
