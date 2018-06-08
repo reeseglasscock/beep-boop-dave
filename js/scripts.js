1d03479d01e6338f7c46adfb702a55ecb4fc4fc2
reeseglasscock <reesegpdx@gmail.com>
2018-06-08 12:42:15 -0700

    split number into array of digits

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,44 @@
 function roboticTranslation(number){
-  var roboticWords = ""
-  console.log(roboticWords)
 
-  if ((number % 3) === 0 && number != 0) {
-    roboticWords = "I’m sorry, Dave. I’m afraid I can’t do that.";
-    return roboticWords;
+  var roboticDigits = number.split("")
+
+  console.log("This is array of numbers" + roboticDigits)
+for (var digit = 0; digit < roboticDigits.length; digit++) {
+  var newArray = []
+  // console.log("This is the digit" + digit)
+  if ((roboticDigits[digit] % 3) === 0 && roboticDigits[digit] != 0) {
+    console.log("This is digit" + roboticDigits[digit])
+    newArray.push("I’m sorry, Dave. I’m afraid I can’t do that.")
+    // console.log("This is the new Array in the push" + newArray)
+    // roboticDigits = "I’m sorry, Dave. I’m afraid I can’t do that.";
+  }
+  else if (roboticDigits[digit] === 1) {
+    console.log(roboticDigits[digit])
+    newArray.push("Boop!");
+    // return newArray;
   }
 
-  else if (number === 1) {
-    roboticWords = "Boop!";
-    return roboticWords;
-  }
+return newArray
+}
+// console.log("newArray after looping:" + newArray)
 
-  else if (number === 0) {
-    roboticWords = "Beep!";
-    return roboticWords;
-    console.log("The number is" + roboticWords)
-  }
 
-  else if (Number.isInteger(number)) {
-    console.log("Does not meet other rules" + number)
-    return number
-  }
-  else {
-    alert("Please enter a number")
-  }
-  return roboticWords;
+
+  //
+  // else if (number === 0) {
+  //   roboticDigits = "Beep!";
+  //   return roboticDigits;
+  //   console.log("The number is" + roboticDigits)
+  // }
+
+  // else if (Number.isInteger(number)) {
+  //   console.log("Does not meet other rules" + number)
+  //   return number
+  // }
+  // else {
+  //   alert("Please enter a number")
+  // }
+  // return roboticDigits;
 }
 
 // console.log(roboticTranslation(0));
@@ -34,8 +47,12 @@ function roboticTranslation(number){
 $(function() {
   $("#robotTalkForm").submit(function(event){
     event.preventDefault();
-    var userInput = parseInt($("#userInput").val());
+    var userInput = $("#userInput").val();
+    roboticTranslation(userInput)
+    // console.log("this is user input" + userInput)
     $("span#outputToUser").text(roboticTranslation(userInput))
+
+    // roboticTranslation(userInput)
   })
 
 })
