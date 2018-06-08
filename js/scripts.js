80ecb6a136e4271e23a00acf36bf8c0aa3607705
reeseglasscock <reesegpdx@gmail.com>
2018-06-08 13:31:22 -0700

    allows number inputted to show if other conditions are not met

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,12 @@
 function roboticTranslation(number){
   var newArray = []
   var roboticDigits = number.split("")
-  console.log("This is roboticDigits:" + roboticDigits)
-  console.log(number)
 
-  // console.log("This is array of numbers" + roboticDigits)
 for (var digit = 0; digit < roboticDigits.length; digit++) {
-console.log("This is the index number of robotic digits" + roboticDigits[digit])
-  // console.log("This is the digit" + digit)
   if (number % 3 == 0 && number != 0) {
-    console.log("This is number" + number)
     newArray.push("I’m sorry, Dave. I’m afraid I can’t do that.")
     break
   }
-
   else if (roboticDigits[digit] == 1) {
     newArray.push("Boop!");
     break
@@ -22,18 +15,16 @@ console.log("This is the index number of robotic digits" + roboticDigits[digit])
     newArray.push("Beep!")
     break
   }
-
+  else {
+    newArray.push(number)
+    break
+  }
 }
 
-
-
-  //
-
-
-  // else if (Number.isInteger(number)) {
+  // else if (Number.isInteger(parseInt($(number)))) {
   //   console.log("Does not meet other rules" + number)
-  //   return number
   // }
+
   // else {
   //   alert("Please enter a number")
   // }
